cd87894954b64cdec70fd336b9f345309226b128
Nicolas Valcarcel <nvalcarcel@gmail.com>
2018-01-15 18:02:06 -0500

    enter optional BPF Filter, print a bunch of package info

--- byos.go
+++ byos.go
@@ -1,55 +1,133 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "time"
-  "github.com/google/gopacket"
-  "github.com/google/gopacket/pcap"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 func devicePicker() string {
-    devices, err := pcap.FindAllDevs()
-    if err != nil {
-      log.Fatal(err)
-    }
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println("Pick a device to scan on:")
-    for n, device := range devices {
-      fmt.Printf("\nNumber: %d :: ", n)
-      fmt.Println("Name: ", device.Name)
-      fmt.Println("Description: ", device.Description)
-      for _, address := range device.Addresses {
-        fmt.Printf("IP address: %s", address.IP)
-        fmt.Printf(" :: ")
-        fmt.Println("Subnet mask: %s", address.Netmask)
-      }
-    }
+	fmt.Println("Pick a device to scan on:")
+	for n, device := range devices {
+		fmt.Printf("\nNumber: %d :: ", n)
+		fmt.Println("Name: ", device.Name)
+		fmt.Println("Description: ", device.Description)
+		for _, address := range device.Addresses {
+			fmt.Printf("IP address: %s", address.IP)
+			fmt.Printf(" :: ")
+			fmt.Println("Subnet mask: %s", address.Netmask)
+		}
+	}
 
-    var choice int
-    fmt.Printf("Enter device number: ")
-    fmt.Scanln(&choice)
+	var choice int
+	fmt.Printf("Enter device number: ")
+	fmt.Scanln(&choice)
 
-    return devices[choice].Name
+	return devices[choice].Name
+}
+
+func setupSniff() *pcap.Handle {
+	device := devicePicker()
+	var timeout time.Duration = -1 * time.Second
+	handle, err := pcap.OpenLive(
+		device,
+		int32(65535),
+		false,
+		timeout,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter BPF Filter (optional): ")
+	rawFilter, _ := reader.ReadString('\n')
+	filter := strings.TrimSpace(rawFilter)
+	fmt.Println("Setting up filter:", filter)
+	handle.SetBPFFilter(filter)
+
+	return handle
+}
+
+// Code from https://www.devdungeon.com/content/packet-capture-injection-and-analysis-gopacket
+func printPacketInfo(packet gopacket.Packet) {
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+		fmt.Println("SRC MAC:", ethernetPacket.SrcMAC)
+		fmt.Println("DST MAC:", ethernetPacket.DstMAC)
+		fmt.Println("ETH Type:", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+		fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+		fmt.Println("Protocol: ", ip.Protocol)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is TCP
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer != nil {
+		fmt.Println("TCP layer detected.")
+		tcp, _ := tcpLayer.(*layers.TCP)
+
+		// TCP layer variables:
+		// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+		// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+		fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+		fmt.Println("Sequence number: ", tcp.Seq)
+		fmt.Println()
+	}
+
+	// Iterate over all layers, printing out each layer type
+	fmt.Println("All packet layers:")
+	for _, layer := range packet.Layers() {
+		fmt.Println("- ", layer.LayerType())
+	}
+
+	// When iterating through packet.Layers() above,
+	// if it lists Payload layer then that is the same as
+	// this applicationLayer. applicationLayer contains the payload
+	applicationLayer := packet.ApplicationLayer()
+	if applicationLayer != nil {
+		fmt.Println("Application layer/Payload found.")
+		fmt.Printf("%s\n", applicationLayer.Payload())
+
+		// Search for a string inside the payload
+		if strings.Contains(string(applicationLayer.Payload()), "HTTP") {
+			fmt.Println("HTTP found!")
+		}
+	}
+
+	// Check for errors
+	if err := packet.ErrorLayer(); err != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+	}
 }
 
 func main() {
-  device := devicePicker()
+	handle := setupSniff()
+	defer handle.Close()
 
-  var timeout time.Duration = -1*time.Second
-  handle, err := pcap.OpenLive(
-    device,
-    int32(65535),
-    false,
-    timeout,
-  )
-  if err != nil {log.Fatal(err)}
-  defer handle.Close()
-
-  handle.SetBPFFilter("arp")
-
-  packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-  for packet := range packetSource.Packets() {
-    fmt.Println(packet)
-  }
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		printPacketInfo(packet)
+	}
 }
